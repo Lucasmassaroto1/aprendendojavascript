9d93822379a232a825b823ba8c0c51576c2906c3
Lucasmassaroto1 <lucasmassaroto17@gmail.com>
2025-05-05 10:52:18 -0300

    concluindo desafio

---
+++ desafios/script.js
@@ -0,0 +1,16 @@
+var a = 10
+var b = 20
+var c = null
+
+document.write('A: ' + a + '<br/>')
+document.write('B: ' + b + '<br/>')
+document.write('Ac: ' + c + '<br/>')
+document.write('<hr/>')
+
+c = a
+a = b
+b = c
+c = null
+document.write('A: ' + a + '<br/>')
+document.write('B: ' + b + '<br/>')
+document.write('c: ' + c + '<br/>')
