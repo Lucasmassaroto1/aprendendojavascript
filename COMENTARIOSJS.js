02492ea923d8bf411d793e29ec30f6e46a6c6f15
Lucasmassaroto1 <lucasmassaroto17@gmail.com>
2025-05-05 09:20:16 -0300

    Adicionando o arquivo de comentarios

---
+++ COMENTARIOSJS.js
@@ -0,0 +1,25 @@
+// _____________ESTE ARQUIVO SERÁ APENAS PARA COMENTÁRIOS SOBRE A LINGUAGEM DE PROGRAMAÇÃO JAVA SCRIPT_____________
+
+/** 
+ * _____________SEU TEXTO AQUI_____________
+ * »
+ * »
+ * »
+ * 
+ */
+
+/** 
+ * _____________TIPOS DE COMENTARIOS_____________
+ * // » Usados Para Comentário De Uma Linha
+ * / * * / » Usados Para Comentários De Múltiplas Linhas
+ * / ** * / » Usados Para Comentários De Blocos / "Documentação"
+ * 
+*/
+
+/** 
+ * _____________TIPOS DE VARIÁVEIS_____________
+ * var » Declara Uma Variável De Função E Pode Ser Reatribuída
+ * let » Declara Uma Variável De Escopo De Bloco E Pode Ser Reatribuída
+ * const » Declara Uma Variável Com Escopo De Bloco E Valor Constante E Não Pode Ser Reatribuída
+ * 
+ */
--- script.js
+++ script.js
@@ -1,2 +1,3 @@
+
 alert("Ola, seja bem vindo para o meu site de aprendizagem")
 document.getElementById('nome').value = 'oi';
