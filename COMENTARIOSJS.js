bc305658efdc4b70bdc4fd660a7680b13c896275
Lucasmassaroto1 <lucasmassaroto17@gmail.com>
2025-05-05 09:40:41 -0300

    mudando comentarios

--- COMENTARIOSJS.js
+++ COMENTARIOSJS.js
@@ -17,9 +17,26 @@
 */
 
 /** 
+ * _____________REGRAS PARA DECLARAÇÃO DE VARIÁVEIS_____________
+ * » Não Podem Ser Iniciadas Com Números, Apenas Com Letras || "_"
+ * » Não Podem Ser Utilizados Caracteres Especias Como "Ç", "^", "~"
+ * » Não Podem Ser Iguais Às Palavras Reservadas Da Linguagem
+ * » JavaScript É Uma Linguagem Case Sensitive, var Ola != De var ola
+ * 
+ */
+
+/** 
  * _____________TIPOS DE VARIÁVEIS_____________
  * var » Declara Uma Variável De Função E Pode Ser Reatribuída
  * let » Declara Uma Variável De Escopo De Bloco E Pode Ser Reatribuída
  * const » Declara Uma Variável Com Escopo De Bloco E Valor Constante E Não Pode Ser Reatribuída
+ * 
+*/
+
+/** 
+ * _____________TIPOS DE DADOS PRIMITIVOS_____________
+ * string » Armazenam Cadeias De Caracteres
+ * number » Armazenam Valores Numericos Positivos || Negativos || Int / Float
+ * boolean » Armazenam Tipos De Estados True || False
  * 
  */
