b2c7bb4a90269c80c089dff49dd9c5991a1c0ae9
Lucasmassaroto1 <lucasmassaroto17@gmail.com>
2025-05-07 11:39:32 -0300

    Calculadora concluida

--- desafios/script.js
+++ desafios/script.js
@@ -15,3 +15,20 @@ document.write('A: ' + a + '<br/>')
 document.write('B: ' + b + '<br/>')
 document.write('c: ' + c + '<br/>') */
 
+function calcular(tipo, valor){
+    if(tipo === 'acao'){
+        if(valor === 'c'){
+            //limpa o visor res
+            document.getElementById('res').value = ''
+        }
+        if(valor === '+' || valor === '-' || valor === '/' || valor === '*' || valor === '.'){
+            document.getElementById('res').value += valor
+        }
+        if(valor === '='){
+            var valor_campo = eval(document.getElementById('res').value)
+            document.getElementById('res').value = valor_campo
+        }
+    } else if(tipo === 'valor'){
+        document.getElementById('res').value += valor
+    }
+}
